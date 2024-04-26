e28557193edd93d2f2e5937fe99ba6861f6b196f
Claudio Jose <claudio.jose.sousaa@gmail.com>
2024-04-26 11:59:10 -0300

    Primeira commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script lang="ts">
+import Banner from './components/Banner.vue';
+import PagConteudo from './components/PagConteudo.vue'
+
+
+export default {
+  components: {
+    Banner,
+    PagConteudo,
+    
+  }
+}
+</script>
+
+
+
+<template>
+  <Banner />
+  <PagConteudo />
+  
+</template>
+
+<style scoped></style>
