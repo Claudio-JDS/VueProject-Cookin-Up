51fac2818526082ae21efe5c875feece50aca1b0
Claudio Jose <claudio.jose.sousaa@gmail.com>
2024-04-27 09:45:58 -0300

    Adicionando eventos

--- src/components/Rodape.vue
+++ src/components/Rodape.vue
@@ -1,6 +1,6 @@
 <template>
   <footer class="rodape paragrafo">
-    Desenvolvido por Alura | 2023 - Projeto fictício sem fins comerciais.
+    Projeto fictício sem fins comerciais.
   </footer>
 </template>
 
